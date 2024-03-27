03c92e08bbc4420e96c192c623944b8344101b3a
Tom Caillaud <caillaud.tom@outlook.fr>
2024-03-27 19:16:02 +0100

    Make sign up view (add several drawables...)

--- app/src/main/java/com/example/paltcg/MainActivity.java
+++ app/src/main/java/com/example/paltcg/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.paltcg;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void debug(android.view.View v) {
+        Log.i("", "debug: clicked");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.app_logo), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.signup);
     }
 }
