9dbabc4ae290a871641a028182d56e7003bdd4ee
Tom Caillaud <caillaud.tom@outlook.fr>
2024-03-27 19:47:15 +0100

    Make gradient animated background

--- app/src/main/java/com/example/paltcg/SignUpActivity.java
+++ app/src/main/java/com/example/paltcg/SignUpActivity.java
@@ -1,35 +1,26 @@
 package com.example.paltcg;
 
-import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.util.Log;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-import java.lang.reflect.Constructor;
+public class SignUpActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    public void debug(android.view.View v) {
-        Log.i("", "debug: clicked");
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.signup);
 
         ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2500);
         animationDrawable.setExitFadeDuration(5000);
         animationDrawable.start();
-
-    }
-
-    public void goSignUpView(android.view.View v) {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
     }
 }
