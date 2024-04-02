173563e6c368d2a410667faf2fe2a9d06db9a0dc
Tom Caillaud <caillaud.tom@outlook.fr>
2024-04-02 20:26:29 +0200

    Fix music handling, modify homepage, start back code

--- app/src/main/java/com/example/paltcg/MainActivity.java
+++ app/src/main/java/com/example/paltcg/MainActivity.java
@@ -4,23 +4,26 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import java.lang.reflect.Constructor;
-
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mainTheme;
-    public void debug(android.view.View v) {
-        Log.i("", "debug: clicked");
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+
+        //TODO: check si compte existant ou non
+        findViewById(R.id.imageView_profilePic).setVisibility(View.GONE);
+        TextView text = (TextView)findViewById(R.id.textView_accountUsername);
+        text.setText(R.string.need_to_create_a_account);
+        findViewById(R.id.textView_completion).setVisibility(View.GONE);
 
         ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
@@ -32,31 +35,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        stopMusic();
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
         playMusic();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopMusic();
+    }
+
     public void goSignUpView(android.view.View v) {
         Intent intent = new Intent(this, SignUpActivity.class);
+        intent.putExtra("music_time", mainTheme.getCurrentPosition());
         startActivity(intent);
     }
 
     private void playMusic() {
-        if (mainTheme == null) {
+        if (mainTheme == null)
             mainTheme = MediaPlayer.create(this, R.raw.title_theme);
-            mainTheme.start();
-        }
+        mainTheme.start();
     }
 
     private void stopMusic() {
         mainTheme.release();
         mainTheme = null;
+    }
+
+    public void leaveApp(android.view.View v) {
+        finish();
+        System.exit(0);
     }
 }
