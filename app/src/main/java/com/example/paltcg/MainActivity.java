f0c92ad46559cf1978dd75e862db49a1ca6d28ae
Tom Caillaud <caillaud.tom@outlook.fr>
2024-04-04 19:18:37 +0200

    Add account creation working (it gives back username, gender and profile pic to main menu)

--- app/src/main/java/com/example/paltcg/MainActivity.java
+++ app/src/main/java/com/example/paltcg/MainActivity.java
@@ -1,29 +1,74 @@
 package com.example.paltcg;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mainTheme;
+    ActivityResultLauncher<Intent> signUpActivityResultLauncher;
+    Boolean accountExists = false;
+
+    Integer profilePicId;
+    String profileUsername;
+    Integer gender; //0 for male, 1 for female
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
 
-        //TODO: check si compte existant ou non
-        findViewById(R.id.imageView_profilePic).setVisibility(View.GONE);
-        TextView text = (TextView)findViewById(R.id.textView_accountUsername);
-        text.setText(R.string.need_to_create_a_account);
-        findViewById(R.id.textView_completion).setVisibility(View.GONE);
+        //TODO: check si sauvegarde existante ou non
+        if (!accountExists) {
+            findViewById(R.id.imageView_profilePic).setVisibility(View.GONE);
+            TextView text = findViewById(R.id.textView_accountUsername);
+            text.setText(R.string.need_to_create_a_account);
+            findViewById(R.id.textView_completion).setVisibility(View.GONE);
+
+            signUpActivityResultLauncher = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        Log.i("TAG", "onActivityResult: received");
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Log.i("TAG", "onActivityResult: received result ok");
+                            Intent resultDatas = result.getData();
+                            if (resultDatas != null) {
+
+                                Log.i("TAG", "onActivityResult: received something");
+                                profilePicId = resultDatas.getIntExtra("profile_pic_id", 0);
+                                profileUsername = resultDatas.getStringExtra("profile_username");
+                                gender = resultDatas.getIntExtra("profile_gender", 0);
+                                Log.i("TAG", "onActivityResult: get those infos : " + profilePicId + " " + profileUsername + " " + gender);
+
+                                ImageView profilePic = findViewById(R.id.imageView_profilePic);
+                                profilePic.setVisibility(View.VISIBLE);
+                                profilePic.setImageResource(getResources()
+                                        .obtainTypedArray(R.array.profils_pics_ids)
+                                        .getResourceId(profilePicId-1,0));
+
+                                TextView text1 = findViewById(R.id.textView_accountUsername);
+                                text1.setText(profileUsername);
+                                findViewById(R.id.textView_completion).setVisibility(View.VISIBLE);
+                            }
+                        }
+                    }
+            );
+        }
+
+
 
         ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
@@ -48,8 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void goSignUpView(android.view.View v) {
         Intent intent = new Intent(this, SignUpActivity.class);
-        intent.putExtra("music_time", mainTheme.getCurrentPosition());
-        startActivity(intent);
+        signUpActivityResultLauncher.launch(intent);
     }
 
     private void playMusic() {
--- app/src/main/java/com/example/paltcg/SignUpActivity.java
+++ app/src/main/java/com/example/paltcg/SignUpActivity.java
@@ -1,15 +1,26 @@
 package com.example.paltcg;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    ImageButton profilePic;
+
+    RadioGroup genders;
+
+    EditText usernameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,9 @@ public class SignUpActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(10);
         animationDrawable.setExitFadeDuration(5000);
         animationDrawable.start();
+
+        genders = findViewById(R.id.radioGroup_gender);
+        usernameEditText = findViewById(R.id.editText_username);
     }
 
     public void goSeeDeckView(android.view.View v) {
@@ -27,5 +41,39 @@ public class SignUpActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DecksActivity.class);
         startActivity(intent);
         Log.i("TAG", "goSeeDeckView: end");
+    }
+
+    public void chooseProfilePic(android.view.View v) {
+        if (profilePic != null) {
+            profilePic.setBackground(null);
+        }
+        profilePic = findViewById(v.getId());
+        profilePic.setBackgroundColor(Color.BLACK);
+    }
+
+    public void signUp(android.view.View v) {
+        if (usernameEditText.getText().toString().isEmpty())
+            Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT).show();
+        else {
+            if (genders.getCheckedRadioButtonId() == -1)
+                Toast.makeText(this, "Please select gender", Toast.LENGTH_SHORT).show();
+            else {
+                if (profilePic == null)
+                    Toast.makeText(this, "Please select a profile picture", Toast.LENGTH_SHORT).show();
+                else {
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("profile_username", usernameEditText.getText().toString());
+                    if (genders.getCheckedRadioButtonId() == R.id.radioButton_genderMale)
+                        returnIntent.putExtra("profile_gender", 0);
+                    else
+                        returnIntent.putExtra("profile_gender", 1);
+                    returnIntent.putExtra("profile_pic_id", Integer.valueOf(profilePic.getTag().toString()));
+                    Log.i("TAG", "signUp: end");
+                    setResult(Activity.RESULT_OK, returnIntent);
+                    finish();
+                }
+            }
+        }
+
     }
 }
