6d3975911031a2ec15b6066d5efcbde656fa5f56
macine <macinebenmohand@gmail.Com>
2024-04-08 00:29:56 +0200

    page stat landscape

--- app/src/main/java/com/example/paltcg/End_Fight_Activity.java
+++ app/src/main/java/com/example/paltcg/End_Fight_Activity.java
@@ -15,10 +15,6 @@ public class End_Fight_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.end_fight);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+;
     }
 }
